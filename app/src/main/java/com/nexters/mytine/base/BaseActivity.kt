3686560287fb1b09502ad1813f92bb640d7ced20
song <0603yang@naver.com>
2020-07-13 19:22:29 +0900

    [Add] Base(Activity,Frgment,ViewModel) 추가

---
+++ app/src/main/java/com/nexters/mytine/base/BaseActivity.kt
@@ -0,0 +1,25 @@
+package com.nexters.mytine.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.annotation.CallSuper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<VB : ViewDataBinding, VM : BaseViewModel> : AppCompatActivity() {
+
+    abstract val layoutId: Int
+    abstract val viewModel: VM
+
+    protected val binding by lazy{
+        DataBindingUtil.setContentView<VB>(this, layoutId)
+    }
+
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        binding.lifecycleOwner = this
+    }
+}
---
+++ app/src/main/java/com/nexters/mytine/base/BaseViewModel.kt
@@ -0,0 +1,7 @@
+package com.nexters.mytine.base
+
+import androidx.lifecycle.ViewModel
+
+class BaseViewModel : ViewModel() {
+
+}
