8bb3f4bace8930d1d73dddc7c094cc5ac6471494
오재환 <ojh102@gmail.com>
2020-07-26 18:50:31 +0900

    feat: stetho 추가

--- app/src/main/java/com/nexters/mytine/MyTineApplication.kt
+++ app/src/main/java/com/nexters/mytine/MyTineApplication.kt
@@ -1,6 +1,7 @@
 package com.nexters.mytine
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.vanniktech.emoji.EmojiManager
 import com.vanniktech.emoji.ios.IosEmojiProvider
 import dagger.hilt.android.HiltAndroidApp
@@ -10,6 +11,9 @@ internal class MyTineApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
         EmojiManager.install(IosEmojiProvider())
     }
 }
