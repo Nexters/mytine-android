f5be03bdfd686a7282a14fb4e092b96ea687c1e1
OH JAE HWAN <ojh102@gmail.com>
2020-08-07 22:27:52 +0900

    refactor: 홈 뷰 정리

--- app/src/main/java/com/nexters/mytine/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nexters/mytine/ui/home/HomeFragment.kt
@@ -3,9 +3,15 @@ package com.nexters.mytine.ui.home
 import android.os.Bundle
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import com.nexters.mytine.R
 import com.nexters.mytine.base.fragment.BaseFragment
 import com.nexters.mytine.databinding.FragmentHomeBinding
+import com.nexters.mytine.ui.home.icongroup.IconGroupAdapter
+import com.nexters.mytine.ui.home.week.WeekAdapter
 import com.nexters.mytine.utils.extensions.observe
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,6 +20,8 @@ internal class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>()
     override val layoutResId = R.layout.fragment_home
     override val viewModelClass = HomeViewModel::class
 
+    private val weekAdapter = WeekAdapter()
+    private val iconGroupAdapter = IconGroupAdapter()
     private val homeAdapter = HomeAdapter()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -21,24 +29,38 @@ internal class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>()
 
         initializeRecyclerView()
 
+        observe(viewModel.weekItems) { weekAdapter.submitList(it) }
+        observe(viewModel.iconGroupItems) { iconGroupAdapter.submitList(it) }
         observe(viewModel.homeItems) { homeAdapter.submitList(it) }
     }
 
     private fun initializeRecyclerView() {
+        binding.rvWeek.run {
+            layoutManager = FlexboxLayoutManager(context, FlexDirection.ROW, FlexWrap.NOWRAP).apply {
+                justifyContent = JustifyContent.SPACE_BETWEEN
+            }
+            adapter = weekAdapter
+        }
+        weekAdapter.setViewHolderViewModel(viewModel)
+
+        binding.rvIconGroup.run {
+            layoutManager = LinearLayoutManager(context)
+            adapter = iconGroupAdapter
+        }
+        iconGroupAdapter.setViewHolderViewModel(viewModel)
+
         binding.rvRoutine.run {
             layoutManager = LinearLayoutManager(context)
             adapter = homeAdapter
+
+            ItemTouchHelper(
+                ItemTouchHelperCallback(object : ItemTouchHelperListener {
+                    override fun onItemSwipe(position: Int) {
+                        viewModel.successRoutine(position)
+                    }
+                })
+            ).attachToRecyclerView(this)
         }
-
-        val itemTouchHelper = ItemTouchHelper(
-            ItemTouchHelperCallback(object : ItemTouchHelperListener {
-                override fun onItemSwipe(position: Int) {
-                    viewModel.successRoutine(position)
-                }
-            })
-        )
-        itemTouchHelper.attachToRecyclerView(binding.rvRoutine)
-
         homeAdapter.setViewHolderViewModel(viewModel)
     }
 }
--- app/src/main/java/com/nexters/mytine/ui/home/icongroup/IconGroupAdapter.kt
+++ app/src/main/java/com/nexters/mytine/ui/home/icongroup/IconGroupAdapter.kt
@@ -4,15 +4,15 @@ import androidx.databinding.ViewDataBinding
 import com.nexters.mytine.R
 import com.nexters.mytine.base.recyclerview.BaseListAdapter
 import com.nexters.mytine.base.recyclerview.BaseViewHolder
-import com.nexters.mytine.databinding.ItemHomeRoutineGroupIconGroupBinding
+import com.nexters.mytine.databinding.ItemHomeIconGroupBinding
 import com.nexters.mytine.ui.home.HomeViewModel
 
 internal class IconGroupAdapter : BaseListAdapter<IconGroupItem>() {
     override fun getItemViewTypeByItem(item: IconGroupItem): Int {
-        return R.layout.item_home_routine_group_icon_group
+        return R.layout.item_home_icon_group
     }
 
     override fun createViewHolder(binding: ViewDataBinding, viewType: Int): BaseViewHolder<IconGroupItem> {
-        return IconGroupViewHolder(binding as ItemHomeRoutineGroupIconGroupBinding, viewModel as HomeViewModel)
+        return IconGroupViewHolder(binding as ItemHomeIconGroupBinding, viewModel as HomeViewModel)
     }
 }
