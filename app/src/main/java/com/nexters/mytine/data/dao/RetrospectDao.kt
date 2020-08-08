fe901906cc3d8ce5c4664252c49510264c8f74b0
wishJinit <wishjinit@gmail.com>
2020-08-08 10:24:46 +0900

    feat : 주간 회고 수행여부 리스트 연결

--- app/src/main/java/com/nexters/mytine/data/dao/RetrospectDao.kt
+++ app/src/main/java/com/nexters/mytine/data/dao/RetrospectDao.kt
@@ -11,6 +11,9 @@ internal abstract class RetrospectDao : BaseDao<Retrospect> {
     @Query("SELECT * FROM retrospect WHERE date=:date")
     abstract suspend fun get(date: LocalDate): Retrospect?
 
+    @Query("SELECT date FROM retrospect WHERE date BETWEEN :from AND :to")
+    abstract fun getRetrospectDatesByDate(from: LocalDate, to: LocalDate): Flow<List<LocalDate>>
+
     @Query("SELECT * FROM retrospect WHERE date=:date")
     abstract fun getFlow(date: LocalDate): Flow<Retrospect?>
 }
