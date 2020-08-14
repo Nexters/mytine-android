3316a4ce80d0a7b9474c8b65a06eabb41248a10f
wishJinit <wishjinit@gmail.com>
2020-08-14 22:12:31 +0900

    fix : week 영어에서 한글로 수정

--- app/src/main/java/com/nexters/mytine/ui/home/week/DayItem.kt
+++ app/src/main/java/com/nexters/mytine/ui/home/week/DayItem.kt
@@ -1,8 +1,14 @@
 package com.nexters.mytine.ui.home.week
 
 import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.*
 
 data class DayItem(
     val date: LocalDate,
     val isRetrospectWrite: Boolean
-)
+) {
+    fun weekName(): String {
+        return date.dayOfWeek.getDisplayName(TextStyle.NARROW, Locale.KOREAN)
+    }
+}
